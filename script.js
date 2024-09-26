956b2551152eb4bf1f77d8c216b2ba586cd9e2c5
TradsKraken <134815285+TradsKraken@users.noreply.github.com>
2024-09-27 01:49:01 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var settingsmenu = document.querySelector(".menu");
+
+var infomenu = document.querySelector(".form")
+
+function menuToggle(){
+
+settingsmenu.classList.toggle("menu-height");
+
+}
+
+function infoToggle(){
+
+infomenu.classList.toggle("form-height");
+
+}
